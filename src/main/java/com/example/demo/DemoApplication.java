6c13d60be66b44e39fb4abb100307d91598aebff
jaixin321 <897162029@qq.com>
2020-09-20 12:56:01 +0800

    提交代码03啊

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,7 @@ public class DemoApplication {
         System.out.printf("123");
         System.out.println("345");
         System.out.println("789");
-        System.out.println("12221");
+        System.out.println("1222dev1");
 
 
     }
