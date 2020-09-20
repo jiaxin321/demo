0e5e40a816ac2a7fc6ba27567d580a5d1d387c42
jaixin321 <897162029@qq.com>
2020-09-20 10:18:13 +0800

    dev 提交代码

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,12 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 
+    public static void main(String[] args) {
+        System.out.printf("123");
+    }
 
 }
